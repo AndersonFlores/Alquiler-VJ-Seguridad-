c0ba547a7856ad45fc592d2f4fd83e274e276f8e
Familia <Familia@DESKTOP-NTLSUTL>
2020-03-20 08:16:05 +1300

    Alquiler VJ (Seguridad)

---
+++ AlquilerVJ/AlquilerVJ.BL/DatosdeInicio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerVJ.BL
+{
+    public class DatosdeInicio : CreateDatabaseIfNotExists<Contexto>
+    {
+        protected override void Seed(Contexto contexto)
+        {
+            var nuevoUsuario = new Usuario();
+            nuevoUsuario.Nombre = "admin";
+            nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("123");
+            contexto.Usuarios.Add(nuevoUsuario);
+
+            var nuevoUsuario2 = new Usuario();
+            nuevoUsuario2.Nombre = "Anderson";
+            nuevoUsuario2.Contrasena = Encriptar.CodificarContrasena("123");
+            contexto.Usuarios.Add(nuevoUsuario2);
+         
+
+            var nuevoUsuario3 = new Usuario();
+            nuevoUsuario3.Nombre = "Oscar";
+            nuevoUsuario3.Contrasena = Encriptar.CodificarContrasena("123");
+            contexto.Usuarios.Add(nuevoUsuario3);
+        
+            var nuevoUsuario4 = new Usuario();
+            nuevoUsuario4.Nombre = "Allan";
+            nuevoUsuario4.Contrasena = Encriptar.CodificarContrasena("123");
+            contexto.Usuarios.Add(nuevoUsuario4);
+           
+
+            var nuevoUsuario5 = new Usuario();
+            nuevoUsuario5.Nombre = "Lucy";
+            nuevoUsuario5.Contrasena = Encriptar.CodificarContrasena("123");
+            contexto.Usuarios.Add(nuevoUsuario5);
+
+            base.Seed(contexto);
+
+        }
+    }
+}
---
+++ AlquilerVJ/AlquilerVJ.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using AlquilerVJ.BL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlquilerVJ.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductosActivos();
+
+            ViewBag.adminWebsiteUrl = 
+                ConfigurationManager.AppSettings["adminWebsiteUrl"]; //Me permite leer el valor que esta en Web.Config de 
+                                                                     //ext.Web y ConfigManager -me permite leer el appSettings
+
+            return View(listadeProductos);
+        }
+    }
+}
